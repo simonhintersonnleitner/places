9774db39838d321d1184c00b488fbaccb3850297
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-05-07 21:17:45 +0200

    fix bootstrap path

--- template/beginHeader.php
+++ template/beginHeader.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="system/bootstrap/css/bootstrap.css">
   <script type="text/javascript" src="system/js/jquery-1.11.0.js"></script>
-  <script type="text/javascript" src="system/js/bootstrap.js"></script>
+  <script type="text/javascript" src="system/bootstrap/js/bootstrap.js"></script>
   <link rel="stylesheet" href="system/leaflet/leaflet.css" />
   <script src="system/leaflet/leaflet.js"></script>
   <link rel="stylesheet" href="system/leaflet/Control.OSMGeocoder.css" />

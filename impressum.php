8ddc5e769a0ad4ef9f586b7121befdadbd2d4192
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2014-05-21 14:31:30 +0200

    added impressum

---
+++ impressum.php
@@ -0,0 +1,30 @@
+<?php
+include 'system/php/functions.php';
+
+
+
+$pagetitle = "Impressum";
+
+include 'template/beginHeader.php';
+?>
+<link rel="stylesheet" type="text/css" href="system/css/index.css">
+<?php
+include 'template/endHeader.php';
+include 'template/menue.php';
+?>
+
+<div class="container">
+  <h3>Impressum</h3>
+  <p>Meine Lieblingsorte ist ein MultiMediaProjekt 1 des Studiengangs MultimediaTechnology der Fachhochschule Salzburg </p><br>
+  <p><b>Simon Hintersonnleiter</b><br>
+    <a href="mailto:shintersonnleitner.mmmt-b2013@fh-salzburg.ac.at">shintersonnleitner.mmmt-b2013@fh-salzburg.ac.at</a></p>
+  <br>
+
+
+</div>
+
+
+
+<?
+include 'template/footer.php';
+?>
--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 </body>
 <footer>
-<div class="container" ><div class="footer"><small><a href="">Impressum</a> | <a href="mailto:shintersonnleitner.mmt-b2013@fh-salzburg.ac.at">Simon Hintersonnleitner</a></small></div></div>
+<div class="container" ><div class="footer"><small><a href="impressum.php">Impressum</a> | <a href="mailto:shintersonnleitner.mmt-b2013@fh-salzburg.ac.at">Simon Hintersonnleitner</a></small></div></div>
 </footer>
 </html>
